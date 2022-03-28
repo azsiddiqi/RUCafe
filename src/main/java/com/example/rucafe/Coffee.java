79ca571fef3a6875b30e54f5b3207f82e872cb37
Karan Patel <karan.patel4@rutgers.edu>
2022-03-27 22:16:23 -0400

    updated coffee

--- src/main/java/com/example/rucafe/Coffee.java
+++ src/main/java/com/example/rucafe/Coffee.java
@@ -1,38 +1,59 @@
 package com.example.rucafe;
 
+import java.util.ArrayList;
+
 public class Coffee extends MenuItem implements Customizable {
 
-    private int numberOfAddIns;
+    private ArrayList<String> totalAddIns;
     private String sizeOfCoffee;
 
     private static final double SHORT_BLACK_COFFEE = 1.69;
     private static final double TALL_BLACK_COFFEE = 2.09;
     private static final double GRANDE_BLACK_COFFEE = 2.49;
     private static final double VENTI_BLACK_COFFEE = 2.89;
+    private static final double ONE_ADD_IN_COST = 0.30;
 
-    public Coffee(int numberOfAddIns, String sizeOfCoffee) {
-        this.numberOfAddIns = numberOfAddIns;
+    public Coffee(ArrayList<String> totalAddIns, String sizeOfCoffee) {
+        this.totalAddIns = totalAddIns;
         this.sizeOfCoffee = sizeOfCoffee;
     }
 
     public boolean add(Object obj){
+        if (obj instanceof String) {
+            String addInName = (String) obj;
+            if (addInName.equals("Cream") || addInName.equals("Syrup") || addInName.equals("Milk") ||
+                    addInName.equals("Caramel") || addInName.equals("Whipped Cream")) {
+                totalAddIns.add(addInName);
+                return true;
+            }
+            return false;
+        }
         return false;
     }
 
     public boolean remove(Object obj){
+        if (obj instanceof String) {
+            String addInName = (String) obj;
+            if (addInName.equals("Cream") || addInName.equals("Syrup") || addInName.equals("Milk") ||
+                    addInName.equals("Caramel") || addInName.equals("Whipped Cream")) {
+                totalAddIns.remove(addInName);
+                return true;
+            }
+            return false;
+        }
         return false;
     }
 
     @Override
     public double itemPrice() {
         if (this.sizeOfCoffee.equals("Short")) {
-            return (this.numberOfAddIns * 0.30) + SHORT_BLACK_COFFEE;
+            return (totalAddIns.size() * ONE_ADD_IN_COST) + SHORT_BLACK_COFFEE;
         } else if (this.sizeOfCoffee.equals("Tall")) {
-            return (this.numberOfAddIns * 0.30) + TALL_BLACK_COFFEE;
+            return (totalAddIns.size() * ONE_ADD_IN_COST) + TALL_BLACK_COFFEE;
         } else if (this.sizeOfCoffee.equals("Grande")) {
-            return (this.numberOfAddIns * 0.30) + GRANDE_BLACK_COFFEE;
+            return (totalAddIns.size() * ONE_ADD_IN_COST) + GRANDE_BLACK_COFFEE;
         } else {
-            return (this.numberOfAddIns * 0.30) + VENTI_BLACK_COFFEE;
+            return (totalAddIns.size() * ONE_ADD_IN_COST) + VENTI_BLACK_COFFEE;
         }
     }
 }
