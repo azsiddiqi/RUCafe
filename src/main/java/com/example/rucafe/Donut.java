606f5a52ddc347c48b80a52ecd3cd316932e83fb
azsid <azsiddiqi52@gmail.com>
2022-03-27 22:01:13 -0400

    donut

--- src/main/java/com/example/rucafe/Donut.java
+++ src/main/java/com/example/rucafe/Donut.java
@@ -1,16 +1,19 @@
 package com.example.rucafe;
 
-public class Donut {
+public class Donut extends MenuItem{
     private double donutPrice;
     private String donutType;
+    private static final double YEAST_DONUT_PRICE = 1.59;
+    private static final double CAKE_DONUT_PRICE = 1.79;
+    private static final double DONUT_HOLE_PRICE = 0.39;
 
     public Donut(String donutType) {
         if (donutType == "Yeast") {
-            this.donutPrice = 1.59;
+            this.donutPrice = YEAST_DONUT_PRICE;
         } else if (donutType == "Cake") {
-            this.donutPrice = 1.79;
+            this.donutPrice = CAKE_DONUT_PRICE;
         } else if (donutType == "Hole") {
-            this.donutPrice = 0.39;
+            this.donutPrice = DONUT_HOLE_PRICE;
         }
     }
 
