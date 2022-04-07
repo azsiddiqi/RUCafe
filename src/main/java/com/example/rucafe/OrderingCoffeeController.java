823e74750cb651e487141b3ce64600473d9f1de8
azsid <azsiddiqi52@gmail.com>
2022-04-07 19:58:55 -0400

    Coffee Controller comments

--- src/main/java/com/example/rucafe/OrderingCoffeeController.java
+++ src/main/java/com/example/rucafe/OrderingCoffeeController.java
@@ -12,6 +12,11 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+/**
+ This class allows the user to order coffee with specifications to the size, quantity, and add-ins of the coffee order.
+ It has a method that adds the coffee order to the users order. seen in "Your Orders, and a method that continuously
+ updates and displays the subtotal of the user's coffee order.
+ */
 public class OrderingCoffeeController implements Initializable {
 
     @FXML
@@ -42,7 +47,13 @@ public class OrderingCoffeeController implements Initializable {
 
     private ArrayList<String> totalAddIns;
 
-
+    /**
+     * After the "Order Coffee" button is pressed in the main menu, this method initializes the data fields in the
+     * "Order Coffee" GUI. It sets the coffee size to "Short" and the quantity to 1, while not selecting any add-ins.
+     * Then it displays the subtotal for this default coffee.
+     * @param location
+     * @param resource
+     */
     @Override
     public void initialize(URL location, ResourceBundle resource){
         totalAddIns = new ArrayList<>();
@@ -53,10 +64,21 @@ public class OrderingCoffeeController implements Initializable {
         subTotal.setText("$1.69");
     }
 
+    /**
+     * Sets the reference of the MainController object to the mainController instance variable, which allows for sharing
+     * data between the MainController object and the OrderingCoffeeController object.
+     * @param controller the MainController object that is passed from the MainController class.
+     */
     public void setMainController(MainController controller) {
         mainController = controller;
     }
 
+    /**
+     * Adds the specified Coffee object to the "Your Order" GUI and then resets the "Order Coffee" GUI data fields to
+     * their default.
+     * @param event An ActionEVent object that occurs when the "Add to Order" button is pressed in the "Ordering Coffee"
+     * GUI.
+     */
     @FXML
     void addToOrder(ActionEvent event) {
         Coffee addCoffee = new Coffee(totalAddIns, listCoffeeSizes.getValue(), listQuantity.getValue());
@@ -72,6 +94,12 @@ public class OrderingCoffeeController implements Initializable {
         subTotal.setText("$1.69");
     }
 
+    /**
+     * This method concurrently updates the subtotal of the coffee order as changes are made, and displays the resulting
+     * total in the subTotal TextField.
+     * @param event An ActionEvent object that occurs when an add-in is selected, when a size is selected, or when a
+     * quantity is selected.
+     */
     @FXML
     void updateSubTotal(ActionEvent event) {
         Coffee currentCoffee = new Coffee(totalAddIns, listCoffeeSizes.getValue(), listQuantity.getValue());
