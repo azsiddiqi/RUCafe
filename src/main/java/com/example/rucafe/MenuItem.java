58e5e6fa2251df56982a244a7b1682ddf9092785
patel <karan.patel4@rutgers.edu>
2022-04-06 23:51:53 -0400

    added javadoc comments

--- src/main/java/com/example/rucafe/MenuItem.java
+++ src/main/java/com/example/rucafe/MenuItem.java
@@ -1,6 +1,15 @@
 package com.example.rucafe;
 
+
+/**
+ This abstract class is extended by the Donut class and the Coffee class, and it contains an abstract method called
+ itemPrice() that returns the price of a Donut or Coffee.
+ */
 public abstract class MenuItem {
 
+    /**
+     Returns the price of a Donut object or a Coffee object, which are both objects of subclasses of this class.
+     @return the price of a Donut object or a Coffee object.
+     */
     public abstract double itemPrice();
 }
